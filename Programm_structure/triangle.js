b2c2651801db06397e8f9a5c88a4a82fcd83d25e
Sasha <skrableskru@gmail.com>
2017-04-28 16:07:02 +0300

    first

---
+++ Programm_structure/triangle.js
@@ -0,0 +1,2 @@
+for (const symbol= '#'; symbol.length>8; symbol++)
+    console.log (symbol)
---
+++ example.js
@@ -0,0 +1,30 @@
+const space= ' '
+const symbol= '#'
+
+
+let arrayOfBoard=[symbol,space]
+        for (var c = 0; c < 9; c++ ) {
+            if(c%2==0) {
+                arrayOfBoard.push(symbol)
+            console.log(arrayOfBoard[c])
+
+            }
+        }
+        
+
+        
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
