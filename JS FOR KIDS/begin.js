17899aa247abcd140dd248b6fe8716afaaffe13f
SashaKa <skrableskru@gmail.com>
2017-09-03 11:48:44 +0300

    some array

--- JS FOR KIDS/begin.js
+++ JS FOR KIDS/begin.js
@@ -11,3 +11,24 @@ console.log(balloons)
 
  const myString = 'эЙ, кАК деЛа?'
  myString[0].toUpperCase() + myString.slice(1).toLowerCase()
+
+
+
+  const randomBodyParts = ['глаз', 'нос', 'череп']
+  const randomAdjectives = ['вонючая', 'унылая', 'дурацкая']
+  const randomPartsOfAnimals = ['шерсть', 'зуб', 'коготь', 'хвост']
+  const randomAnimals = ['кота', 'бкгемота', 'касатки', 'ежа', 'Ламы', 'собаки']
+  const randomPartsOfAnimal = [randomPartsOfAnimals[Math.floor(Math.random() * randomPartsOfAnimals.length)]]
+  const randomAnimal = [randomAnimals[Math.floor(Math.random() * randomAnimals.length)]]
+  const randomBodyPart = randomBodyParts[Math.floor(Math.random() * randomBodyParts.length)]
+  const randomAdjective = randomAdjectives[Math.floor(Math.random() * 3)]
+  const randomInsult = ['У тебя', randomBodyPart, 'еще более', randomAdjective, 'чем', randomPartsOfAnimal, 'у', randomAnimal + '!!!'].join(' ')
+
+
+   const numbers = [3, 2, 1]
+   console.log(numbers.join(' больше, чем '))
+
+
+
+   var myCrazyObject = { name: 'Нелепый объект', 'some array': [7, 9, { purpose: 'путаница', number: 123 }, 3.3], 'random animal': 'Банановая акула' }
+   console.log(myCrazyObject['some array'][2].number)
